b3f67965a432e50b3b348754ff649ea07962c8db
Yunok <ahyz0569@gmail.com>
2019-12-12 10:29:34 +0900

    html로 응답하기

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
@@ -15,5 +15,14 @@ def hi():
 def yunok():
     return 'hello yunok'
 
+@app.route('/html_tag')
+def html_tag():
+    return '<h1>안녕하세요</h1>'
+
+@app.route('/html_file')
+def html_file():
+    return render_template('index.html')
+
 if __name__ == '__main__':
+    # debug=True: 개발자 모드를 켜놔서 저장할때마다 서버에 반영이됨
     app.run(debug=True)
